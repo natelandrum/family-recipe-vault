5f6cecef0724b1b53b1242c5869fe5639062fdc4
Lenora Stevens <tlbs6513@gmail.com>
2025-01-31 20:57:46 -0600

    added Link to the recipe detail page in user and recipes

--- app/components/profile/RecipeList.tsx
+++ app/components/profile/RecipeList.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import { Recipe } from "@/app/lib/definitions";
+import Link from "next/link";
+import RecipesList from "../public_recipes/RecipesList";
 
 interface RecipeListProps {
     recipes: Recipe[];
 }
-
+console.log(RecipesList)
 const RecipeList: React.FC<RecipeListProps> = ({ recipes }) => {
     return (
         <section className="p-4 bg-gray-100 rounded-lg mb-6">
@@ -14,10 +16,13 @@ const RecipeList: React.FC<RecipeListProps> = ({ recipes }) => {
             {recipes.length > 0 ? (
                 <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                     {recipes.map(recipe => (
+                        <Link key={recipe.recipe_id}href={`../../recipe/${recipe.recipe_id}`}>
                         <li key={recipe.recipe_id} className="bg-white p-4 rounded-lg shadow">
                             <h3 className="text-xl font-semibold">{recipe.recipe_name}</h3>
                             <p>{recipe.recipe_description}</p>
                         </li>
+                        </Link>
+
                     ))}
                 </ul>
             ) : (
--- app/components/public_recipes/RecipeCard.tsx
+++ app/components/public_recipes/RecipeCard.tsx
@@ -1,4 +1,5 @@
 import { Recipe } from "@/app/lib/definitions";
+import Link from "next/link";
 
 interface RecipeCardProps {
   recipe: Recipe;
@@ -7,6 +8,7 @@ interface RecipeCardProps {
 export default function RecipeCard({ recipe }: RecipeCardProps) {
   return (
     <div className="border rounded-lg p-4 shadow-md">
+      <Link href={`../../recipe/${recipe.recipe_id}`}>
       <h2 className="text-xl font-bold">{recipe.recipe_name}</h2>
       <p className="text-gray-600">{recipe.recipe_description}</p>
       {recipe.recipe_image && (
@@ -16,6 +18,7 @@ export default function RecipeCard({ recipe }: RecipeCardProps) {
         className="w-full h-48 object-cover rounded-md mt-2"
     />
       )}
+    </Link>
     </div>
   );
 }
