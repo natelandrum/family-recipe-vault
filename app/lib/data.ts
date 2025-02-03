0c5568647256e899de9da9ae4563410c8b9cdbdf
natelandrum <nate178dawg@gmail.com>
2025-02-03 09:52:32 -0600

    Get editable recipe

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { sql } from "@vercel/postgres";
-import { Family, Recipe, RecipeWithAuthor } from "./definitions";
+import { Family, Ingredient, Recipe, RecipeWithAuthor } from "./definitions";
 
 
 export async function getRecipesByUser(userId: number): Promise<Recipe[]> {
@@ -14,28 +14,46 @@ export async function getRecipesByUser(userId: number): Promise<Recipe[]> {
     }
 }
 
-export async function getRecipeTags(recipeId: number): Promise<string[]> {
+export async function getRecipeById(recipeId: number): Promise<Recipe> {
+    try {
+        const response = await sql`SELECT * FROM recipe WHERE recipe_id = ${recipeId}`;
+        return response.rows[0] as Recipe;
+    } catch (error) {
+        console.error("Error fetching recipe by id:", error);
+        throw new Error("Failed to fetch recipe by id");
+    }
+}
+
+export async function getRecipeTags(recipeId: number): Promise<{ tagName: string }[]> {
     try {
         const response = await sql`
-        SELECT tag_name FROM tag 
-        JOIN recipe_tag 
-        ON tag.tag_id = recipe_tag.tag_id 
+        SELECT tag_name FROM tags
+        JOIN recipe_tags 
+        ON tags.tag_id = recipe_tags.tag_id 
         WHERE recipe_id = ${recipeId}`;
-        return response.rows.map(row => row.tag_name);
+        return response.rows.map(row => ({ tagName: row.tag_name }));
     } catch (error) {
         console.error("Error fetching recipe tags:", error);
         throw new Error("Failed to fetch recipe tags");
     }
 }
 
-export async function getRecipeIngredients(recipeId: number): Promise<string[]> {
+export async function getRecipeIngredients(recipeId: number): Promise<Ingredient[]> {
     try {
         const response = await sql`
-        SELECT ingredient_name FROM ingredient 
-        JOIN recipe_ingredient 
-        ON ingredient.ingredient_id = recipe_ingredient.ingredient_id 
+        SELECT ingredients.ingredient_id, ingredient_name, quantity, unit, preparation_method FROM ingredients 
+        JOIN recipe_ingredients 
+        ON ingredients.ingredient_id = recipe_ingredients.ingredient_id 
         WHERE recipe_id = ${recipeId}`;
-        return response.rows.map(row => row.ingredient_name);
+        return response.rows.map(row => {
+            return {
+                ingredient_id: row.ingredient_id,
+                ingredient_name: row.ingredient_name,
+                quantity: row.quantity,
+                unit: row.unit,
+                preparation_method: row.preparation_method
+            };
+        })
     } catch (error) {
         console.error("Error fetching recipe ingredients:", error);
         throw new Error("Failed to fetch recipe ingredients");
@@ -77,5 +95,21 @@ export async function getFamilyGroupByUser(userId: number): Promise<Family[]> {
     } catch (error) {
         console.error("Error fetching family group by user:", error);
         throw new Error("Failed to fetch family group by user");
+    }
+}
+
+export async function getEditFields(recipeId: number) {
+    try {
+        const recipe = await getRecipeById(recipeId);
+        const tags = await getRecipeTags(recipeId);
+        const ingredients = await getRecipeIngredients(recipeId);
+        return {
+            recipe,
+            ingredients,
+            tags            
+        }
+    } catch (error) {
+        console.error("Error fetching edit fields:", error);
+        throw new Error("Failed to fetch edit fields");
     }
 }
