c93e888ff389c90c19dad70e42e4883374ff8224
natelandrum <nate178dawg@gmail.com>
2025-02-12 06:33:04 -0600

    Featured recipes from cloud

--- app/components/home/FeaturedRecipes.tsx
+++ app/components/home/FeaturedRecipes.tsx
@@ -1,26 +1,8 @@
-import Image from "next/image";
+import RecipeCard from "../public_recipes/RecipeCard";
+import { getFeaturedRecipes } from "@/app/lib/data";
 
-export default function FeaturedRecipes() {
-  const recipes = [
-    {
-      name: "Grandma's Apple Pie",
-      image: "/apple-pie.jpg",
-      description: "A delicious family favorite.",
-      alt: "Apple Pie - Photo by Priscilla Du Preez",
-    },
-    {
-      name: "Classic Lasagna",
-      image: "/lasagna.jpg",
-      description: "Perfect for gatherings.",
-      alt: "Photo by Karolina Kołodziejczak",
-    },
-    {
-      name: "Homemade Tacos",
-      image: "/tacos.jpg",
-      description: "A taste of home.",
-      alt: "Photo by Evan Wise",
-    },
-  ];
+export default async function FeaturedRecipes() {
+  const recipes = await getFeaturedRecipes();
 
   return (
     <section className="py-16 bg-[var(--color-primary)] text-center">
@@ -28,24 +10,10 @@ export default function FeaturedRecipes() {
         Featured Recipes
       </h2>
       <div className="max-w-6xl mx-auto px-6 grid md:grid-cols-3 gap-8">
-        {recipes.map(({ name, image, description, alt }) => (
+        {recipes.map((recipe) => (
           <div
-            key={name}
-            className="bg-white p-8 rounded-lg shadow-lg hover:shadow-2xl transition duration-300"
-          >
-            <div className="relative w-40 h-40 mx-auto">
-              <Image
-                src={image}
-                alt={alt}
-                fill={true}
-                className="rounded-full object-cover"
-              />
-            </div>
-
-            <div className="p-6">
-              <h3 className="text-3xl text-[var(--color-dark)] font-bold">{name}</h3>
-              <p className="text-gray-600 mt-2">{description}</p>
-            </div>
+            key={recipe.recipe_id}>
+            <RecipeCard recipe={recipe} />
           </div>
         ))}
       </div>
